57c7746115446c97ffd075b4eedbb7b324305fc7
Daniel Zeman <ledaniel.zeman@gmail.com>
2017-10-14 22:51:28 +0200

    171014 - prettify js data

--- src/data/data.js
+++ src/data/data.js
@@ -23,17 +23,17 @@ module.exports = [
 			{
 				title: "Why my discount dissapeard from the list?",
 				answer: `<p>There are 3 possibilities why the disount is dissapeared from the list</p>
-							<ol style="list-style-type: lower-alpha">
-								<li>The discount is expired and only can be found in the expired list;</li>
-								<li>The discount is deleted by merchants</li>
-								<li>The discount is deleted by HCC</li>
-							</ol>
-							<ul style="list-style-type: none">
-								<li>For <em>a)</em>, please check the expired list</li>
-								<li>For <em>b)</em>, please discuss whether the discount is deleted internally</li>
-								<li>For <em>c)</em>. Deleted by HCC are because of the failed discount validation on operation side.
-								Please make sure no sensitive or illegal information contains in discount and submit again</li>
-							</ul> `
+						<ol style="list-style-type: lower-alpha">
+							<li>The discount is expired and only can be found in the expired list;</li>
+							<li>The discount is deleted by merchants</li>
+							<li>The discount is deleted by HCC</li>
+						</ol>
+						<ul style="list-style-type: none">
+							<li>For <em>a)</em>, please check the expired list</li>
+							<li>For <em>b)</em>, please discuss whether the discount is deleted internally</li>
+							<li>For <em>c)</em>. Deleted by HCC are because of the failed discount validation on operation side.
+							Please make sure no sensitive or illegal information contains in discount and submit again</li>
+						</ul> `
 			},
 			{
 				title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
@@ -57,11 +57,11 @@ module.exports = [
 			{
 				title: "Why my discount dissapeard from the gist?",
 				answer: `<p>amet, consectetur adipisicing elit. A, accusantium aliquid aspernatur corporis </p>
-							<ol style="list-style-type: lower-alpha">
-								<li>eos eveniet ex hic id in maxime, nam praesentium quas, quos </li>
-								<li>repellat reprehenderit soluta ut vel veritatis.</li>
-							</ol>
-							<p>amet, consectetur adipisicing elit. A, accusantium aliquid aspernatur corporis </p> `
+						<ol style="list-style-type: lower-alpha">
+							<li>eos eveniet ex hic id in maxime, nam praesentium quas, quos </li>
+							<li>repellat reprehenderit soluta ut vel veritatis.</li>
+						</ol>
+						<p>amet, consectetur adipisicing elit. A, accusantium aliquid aspernatur corporis </p> `
 			},
 			{
 				title: "Consectetur adipisicing elit?",
@@ -71,11 +71,11 @@ module.exports = [
 			{
 				title: "Why am I so awesome?",
 				answer: `Well here is a list
-							<ol>
-								<li>Cause I can loop</li>
-								<li>Cause I can loop</li>
-								<li>Cause I can loop</li>
-							</ol>`
+						<ol>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+						</ol>`
 			}
 		]
 	},
@@ -85,11 +85,11 @@ module.exports = [
 			{
 				title: "Why my discount dissapeard from the gist?",
 				answer: `<p>amet, consectetur adipisicing elit. A, accusantium aliquid aspernatur corporis </p>
-													<ol style="list-style-type: lower-alpha">
-														<li>eos eveniet ex hic id in maxime, nam praesentium quas, quos </li>
-														<li>repellat reprehenderit soluta ut vel veritatis.</li>
-													</ol>
-													<p>amet, consectetur adipisicing elit. A, accusantium aliquid aspernatur corporis </p> `
+						<ol style="list-style-type: lower-alpha">
+							<li>eos eveniet ex hic id in maxime, nam praesentium quas, quos </li>
+							<li>repellat reprehenderit soluta ut vel veritatis.</li>
+						</ol>
+						<p>amet, consectetur adipisicing elit. A, accusantium aliquid aspernatur corporis </p> `
 			},
 			{
 				title: "Consectetur adipisicing elit?",
@@ -99,11 +99,11 @@ module.exports = [
 			{
 				title: "Why am I so awesome?",
 				answer: `Well here is a list
-													<ol>
-														<li>Cause I can loop</li>
-														<li>Cause I can loop</li>
-														<li>Cause I can loop</li>
-													</ol>`
+						<ol>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+						</ol>`
 			}
 		]
 	},
@@ -113,17 +113,17 @@ module.exports = [
 			{
 				title: "Why my discount dissapeard from the list?",
 				answer: `<p>There are 3 possibilities why the disount is dissapeared from the list</p>
-													<ol style="list-style-type: lower-alpha">
-														<li>The discount is expired and only can be found in the expired list;</li>
-														<li>The discount is deleted by merchants</li>
-														<li>The discount is deleted by HCC</li>
-													</ol>
-													<ul style="list-style-type: none">
-														<li>For <em>a)</em>, please check the expired list</li>
-														<li>For <em>b)</em>, please discuss whether the discount is deleted internally</li>
-														<li>For <em>c)</em>. Deleted by HCC are because of the failed discount validation on operation side.
-														Please make sure no sensitive or illegal information contains in discount and submit again</li>
-													</ul> `
+						<ol style="list-style-type: lower-alpha">
+							<li>The discount is expired and only can be found in the expired list;</li>
+							<li>The discount is deleted by merchants</li>
+							<li>The discount is deleted by HCC</li>
+						</ol>
+						<ul style="list-style-type: none">
+							<li>For <em>a)</em>, please check the expired list</li>
+							<li>For <em>b)</em>, please discuss whether the discount is deleted internally</li>
+							<li>For <em>c)</em>. Deleted by HCC are because of the failed discount validation on operation side.
+							Please make sure no sensitive or illegal information contains in discount and submit again</li>
+						</ul> `
 			},
 			{
 				title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
@@ -133,26 +133,26 @@ module.exports = [
 			{
 				title: "Why am I so awesome?",
 				answer: `Well here is a list
-												<ol>
-													<li>Cause I can loop</li>
-													<li>Cause I can loop</li>
-													<li>Cause I can loop</li>
-												</ol>`
+						<ol>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+						</ol>`
 			},
 			{
 				title: "Why my discount dissapeard from the list?",
 				answer: `<p>There are 3 possibilities why the disount is dissapeared from the list</p>
-																			<ol style="list-style-type: lower-alpha">
-																				<li>The discount is expired and only can be found in the expired list;</li>
-																				<li>The discount is deleted by merchants</li>
-																				<li>The discount is deleted by HCC</li>
-																			</ol>
-																			<ul style="list-style-type: none">
-																				<li>For <em>a)</em>, please check the expired list</li>
-																				<li>For <em>b)</em>, please discuss whether the discount is deleted internally</li>
-																				<li>For <em>c)</em>. Deleted by HCC are because of the failed discount validation on operation side.
-																				Please make sure no sensitive or illegal information contains in discount and submit again</li>
-																			</ul> `
+						<ol style="list-style-type: lower-alpha">
+							<li>The discount is expired and only can be found in the expired list;</li>
+							<li>The discount is deleted by merchants</li>
+							<li>The discount is deleted by HCC</li>
+						</ol>
+						<ul style="list-style-type: none">
+							<li>For <em>a)</em>, please check the expired list</li>
+							<li>For <em>b)</em>, please discuss whether the discount is deleted internally</li>
+							<li>For <em>c)</em>. Deleted by HCC are because of the failed discount validation on operation side.
+							Please make sure no sensitive or illegal information contains in discount and submit again</li>
+						</ul> `
 			},
 			{
 				title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
@@ -162,11 +162,11 @@ module.exports = [
 			{
 				title: "Why am I so awesome?",
 				answer: `Well here is a list
-																		<ol>
-																			<li>Cause I can loop</li>
-																			<li>Cause I can loop</li>
-																			<li>Cause I can loop</li>
-																		</ol>`
+						<ol>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+							<li>Cause I can loop</li>
+						</ol>`
 			}
 		]
 	},
